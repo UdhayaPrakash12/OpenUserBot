6c1932467f2cf653306f6bca5cab6f87daf349d2
UdhayaPrakash12 <udhayaprakash1999@gmail.com>
2020-08-24 12:45:36 +0530

    Removed emojis from alive and covid modules
    Signed-off-by: UdhayaPrakash12 <udhayaprakash1999@gmail.com>

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -14,14 +14,14 @@ async def corona(event):
     covid = Covid(source="worldometers")
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`⚠️Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
-        output_text += f"`☢️Active      : {country_data['active']}`\n"
-        output_text += f"`🤕Critical    : {country_data['critical']}`\n"
-        output_text += f"`😟New Deaths  : {country_data['new_deaths']}`\n\n"
-        output_text += f"`⚰️Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
-        output_text += f"`😔New Cases   : {country_data['new_cases']}`\n"
-        output_text += f"`😇Recovered   : {country_data['recovered']}`\n"
-        output_text += f"`🧪Total tests : {country_data['total_tests']}`\n\n"
+        output_text =  f"`Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
+        output_text += f"`Active      : {country_data['active']}`\n"
+        output_text += f"`Critical    : {country_data['critical']}`\n"
+        output_text += f"`New Deaths  : {country_data['new_deaths']}`\n\n"
+        output_text += f"`Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
+        output_text += f"`New Cases   : {country_data['new_cases']}`\n"
+        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text += f"`Total tests : {country_data['total_tests']}`\n\n"
         output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
     else:
         output_text = "No information yet about this country!"
@@ -35,14 +35,14 @@ async def corona(event):
     covid = Covid(source="worldometers")
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`⚠️Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
-        output_text += f"`☢️Active      : {country_data['active']}`\n"
-        output_text += f"`🤕Critical    : {country_data['critical']}`\n"
-        output_text += f"`😟New Deaths  : {country_data['new_deaths']}`\n\n"
-        output_text += f"`⚰️Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
-        output_text += f"`😔New Cases   : {country_data['new_cases']}`\n"
-        output_text += f"`😇Recovered   : {country_data['recovered']}`\n"
-        output_text += "`🧪Total tests : N/A`\n\n"
+        output_text =  f"`Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
+        output_text += f"`Active      : {country_data['active']}`\n"
+        output_text += f"`Critical    : {country_data['critical']}`\n"
+        output_text += f"`New Deaths  : {country_data['new_deaths']}`\n\n"
+        output_text += f"`Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
+        output_text += f"`New Cases   : {country_data['new_cases']}`\n"
+        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text += "`Total tests : N/A`\n\n"
         output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
     else:
         output_text = "No information yet about this country!"
